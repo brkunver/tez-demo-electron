3e8375629f4878058bb22162b554628a3d5bf2ba
burak <burakhanunver@gmail.com>
2023-05-01 13:31:12 +0300

    v2

--- src/videoList.js
+++ src/videoList.js
@@ -1,14 +1,120 @@
+// Videoların konumlarını içeren listeler
+
 let neutralVideos = [
-  "videos/neutral/neu01vx.mp4",
-  "videos/neutral/neu02vx.mp4",
-  "videos/neutral/neu03vx.mp4",
-  "videos/neutral/neu04vx.mp4",
-  "videos/neutral/neu05vx.mp4",
-  "videos/neutral/neu06vx.mp4",
-  "videos/neutral/neu07vx.mp4",
-  "videos/neutral/neu08vx.mp4",
-  // "videos/neutral/neu09vx.mp4", neu09vx video elementi tarafından tanınmadı
-  "videos/neutral/neu10vx.mp4",
-  "videos/neutral/neu11vx.mp4",
-  "videos/neutral/neu12vx.mp4",
+  "videos/neu02vx.mp4",
+  "videos/neu03vx.mp4",
+  "videos/neu01vx.mp4",
+  "videos/neu04vx.mp4",
+  "videos/neu05vx.mp4",
+  "videos/neu06vx.mp4",
+  "videos/neu07vx.mp4",
+  "videos/neu08vx.mp4",
+  // "videos/neu09vx.mp4", neu09vx video elementi tarafından tanınmadı
+  "videos/neu10vx.mp4",
+  "videos/neu11vx.mp4",
+  "videos/neu12vx.mp4",
+]
+
+let amusementVideos = [
+  //"videos/amu01vp.mp4",
+  //"videos/amu02vp.mp4",
+  "videos/amu03vp.mp4",
+  "videos/amu04vp.mp4",
+  "videos/amu05vp.mp4",
+  "videos/amu06vp.mp4",
+  "videos/amu07vp.mp4",
+  "videos/amu08vp.mp4",
+  //"videos/amu09vp.mp4",
+  "videos/amu10vp.mp4",
+]
+
+let angryVideos = [
+  "videos/ang01vn.mp4",
+  "videos/ang02vn.mp4",
+  "videos/ang04vn.mp4",
+  "videos/ang06vn.mp4",
+  "videos/ang07vn.mp4",
+  "videos/ang08vn.mp4",
+  "videos/ang09vn.mp4",
+  "videos/ang10vn.mp4",
+]
+
+let calmVideos = [
+  "videos/cal02vp.mp4",
+  "videos/cal03vp.mp4",
+  "videos/cal04vp.mp4",
+  "videos/cal05vp.mp4",
+  "videos/cal07vp.mp4",
+  "videos/cal08vp.mp4",
+  "videos/cal09vp.mp4",
+  "videos/cal10vp.mp4",
+  "videos/cal11vp.mp4",
+]
+
+let cmpVideos = [
+  "videos/cmp01vp.mp4",
+  "videos/cmp03vp.mp4",
+  "videos/cmp04vp.mp4",
+  "videos/cmp05vp.mp4",
+  "videos/cmp06vp.mp4",
+  "videos/cmp08vn.mp4",
+  "videos/cmp09vn.mp4",
+  "videos/cmp10vn.mp4",
+]
+
+let disgustingVideos = [
+  "videos/dis01vn.mp4",
+  "videos/dis02vn.mp4",
+  "videos/dis03vn.mp4",
+  "videos/dis05vn.mp4",
+  "videos/dis06vn.mp4",
+  "videos/dis07vn.mp4",
+  "videos/dis08vn.mp4",
+  "videos/dis09vn.mp4",
+  "videos/dis10vn.mp4",
+]
+
+let fearVideos = [
+  "videos/fea01vn.mp4",
+  "videos/fea02vn.mp4",
+  "videos/fea03vn.mp4",
+  "videos/fea05vn.mp4",
+  "videos/fea06vn.mp4",
+  "videos/fea07vn.mp4",
+  "videos/fea08vn.mp4",
+  "videos/fea09vn.mp4",
+  "videos/fea10vn.mp4",
+]
+
+let horrorVideos = [
+  "videos/horgif1gn.mp4",
+  "videos/horgif2gn.mp4",
+  "videos/horgif3gn.mp4",
+  "videos/horgif4gn.mp4",
+  "videos/horgif5gn.mp4",
+  "videos/horgif6gn.mp4",
+  "videos/horgif7gn.mp4",
+  "videos/horgif8gn.mp4",
+  "videos/horgif9gn.mp4",
+]
+
+let sadVideos = [
+  "videos/sad01vn.mp4",
+  "videos/sad03vn.mp4",
+  "videos/sad04vn.mp4",
+  "videos/sad05vn.mp4",
+  "videos/sad07vn.mp4",
+  "videos/sad08vn.mp4",
+  "videos/sad09vn.mp4",
+]
+
+let tenderVideos = [
+  "videos/ten01vp.mp4",
+  "videos/ten02vp.mp4",
+  "videos/ten03vp.mp4",
+  "videos/ten04vp.mp4",
+  "videos/ten06vp.mp4",
+  "videos/ten07vp.mp4",
+  "videos/ten08vp.mp4",
+  "videos/ten09vp.mp4",
 ]
